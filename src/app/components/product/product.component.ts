684c5a6c7a75e3cc429907035d5829cd5c6b8b84
sancar bal <sncr.bl@hotmail.com>
2021-12-16 17:20:50 +0300

    Angular Day 2

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Prodcut } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Prodcut[] = [];
+  dataLoaded:boolean = false;
+
+  constructor(private productService:ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    })
+  }
+}
---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export interface Prodcut{
+    productId:number;
+    categoryId:number;
+    productName:string;
+    unitInStock:number;
+    unitPrice:number;
+}
